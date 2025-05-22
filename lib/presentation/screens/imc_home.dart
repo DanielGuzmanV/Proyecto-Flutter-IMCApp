b69628e888c055337699da8fa7173833b40cb54d
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-21 22:05:53 -0400

    se agrego el boton para calcular

--- lib/presentation/screens/imc_home.dart
+++ lib/presentation/screens/imc_home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:imc_app/components/gender_selector.dart';
 import 'package:imc_app/components/height_selector.dart';
 import 'package:imc_app/components/number_selector.dart';
+import 'package:imc_app/config/theme/app_theme.dart';
+import 'package:imc_app/config/theme/text_style.dart';
 
 class ImcHomeScreen extends StatefulWidget {
   const ImcHomeScreen({super.key});
@@ -17,18 +19,20 @@ class _ImcHomeScreenState extends State<ImcHomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return Column(
       children: <Widget> [
 
         // Clases:
         const GenderSelector(),
         const HeightSelector(),
 
+        // widgets de edad y peso:
         Padding(
           padding: const EdgeInsets.all(16),
           child: Row(
             children: <Widget> [
           
+              // Clase para seleccionar el peso:
               Expanded(
                 child: NumberSelector(
                   title: 'PESO', 
@@ -49,6 +53,7 @@ class _ImcHomeScreenState extends State<ImcHomeScreen> {
           
               const SizedBox(width: 16,),
               
+              // Clase para seleccionar la edad:
               Expanded(
                 child: NumberSelector(
                   title: 'EDAD', 
@@ -66,10 +71,35 @@ class _ImcHomeScreenState extends State<ImcHomeScreen> {
                   },
                 )
               ),
-            
             ],
           ),
         ),
+
+        const Spacer(),
+
+        // Clase para el boton de calcular:
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: SizedBox(
+            height: 60,
+            width: double.infinity,
+
+            child: ElevatedButton(
+              onPressed: () {}, 
+              style: ButtonStyle(
+                shape: WidgetStateProperty.all(
+                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                ),
+                backgroundColor: WidgetStateProperty.all(ThemeColor.primary)
+              ),
+              child: const Text(
+                'Calcular',
+                style: ClaseTextStyles.bodyText,
+              )
+            ),
+          ),
+        ),
+
       ],
     );
   }
